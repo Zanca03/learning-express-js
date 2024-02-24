404ac5da30933a9c06c721fd9e03395f1aa8a200
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2024-02-24 16:00:41 -0300

    Learn about HTTP request GET with Express

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port,() =>{ 
+    console.log("Server is running!");
+})
+
+app.get("/",(req,res) =>{
+    res.send("<h1>Hello World!</h1>");
+})
+
+app.get("/contact",(req,res) => {
+    res.send("My emails is carlos.pereira@fake.com.br");
+})
