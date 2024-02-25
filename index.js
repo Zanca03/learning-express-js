ec92a92984da26d75ef2b7ba9ad3a32ac470b1f8
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2024-02-25 10:31:40 -0300

    Learn about Custom Logging and with Morgan

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+function logger(req,res,next) { //Custom Logging
+    console.log("Request method: " + req.method);
+    console.log("Request URL: " + req.url);
+    next();
+};
+
+app.use(logger);
+
+app.use(morgan("dev")); //Using Morgan to Log
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,6 +31,6 @@ app.get("/",(req,res) =>{
 })
 
 app.post("/submit",(req,res) => {
-    console.log(req.body);
+    res.send(`<h1>Well Done, that's your band name:</h1><h3>${req.body.pet}${req.body.street}</h3>`);
     res.status(200);
 })
