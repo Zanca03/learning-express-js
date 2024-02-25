6b9fe693160c1c1e1851ec0dd7270d546da3df6e
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2024-02-24 22:19:23 -0300

    Learn about Middlewares

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port,() =>{ 
     console.log("Server is running!");
 })
 
 app.get("/",(req,res) =>{
-    res.send("<h1>Hello World!</h1>");
+    res.sendFile(__dirname + "/public/index.html");
+    res.status(200);
 })
 
-app.get("/contact",(req,res) => {
-    res.send("My emails is carlos.pereira@fake.com.br");
+app.post("/submit",(req,res) => {
+    console.log(req.body);
+    res.status(200);
 })
